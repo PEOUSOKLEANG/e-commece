e3fe823993dc7f5c31c52192d3d2fc2a106257de
PEOUSOKLEANG <sokleang34802@gmail.com>
2023-03-16 09:01:46 +0700

    First initialize project
    *Homepage
    *Aboutpage
    *Productspage
    *Checkout

---
+++ E-commerce/src/views/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+  
+      <div class="wrapper">
+        <HelloWorld msg="You did it!" />
+  
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+          <RouterLink to="/product">product</RouterLink>
+          <button @click="$emit"> </button>
+        </nav>
+      </div>
+    </header>
+  
+    <RouterView />
+  </template>
