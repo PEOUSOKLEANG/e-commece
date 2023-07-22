e21130a5ea2bca79d6115168f8c254cf57b9eee4
PEOUSOKLEANG <sokleang34802@gmail.com>
2023-07-22 19:12:31 +0700

    lastUpdate

--- SportShop/src/main.js
+++ SportShop/src/main.js
@@ -1,8 +1,8 @@
+// import './assets/main.css' dont open it because have error
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
-
 
 const app = createApp(App)
 
